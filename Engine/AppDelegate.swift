6c64072b355e996065c106cd627d56eca7c6b673
Farkladin <awasorara@gmail.com>
2026-03-14 21:34:29 +0900

    Create AppDelegate.swift

---
+++ Engine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Engine
+//
+//  Created by Sora Sugiyama on 3/9/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
