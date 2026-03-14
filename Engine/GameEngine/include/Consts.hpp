0a9e5777ac3d359e09f3b9a90eaa53c5809994a6
Farkladin <awasorara@gmail.com>
2026-03-14 21:39:26 +0900

    Add files via upload

---
+++ Engine/GameEngine/include/Consts.hpp
@@ -0,0 +1,17 @@
+//
+//  Consts.hpp
+//  LetsUseSwift
+//
+//  Created by Sora Sugiyama on 2/19/26.
+//
+
+#ifndef Consts_h
+#define Consts_h
+
+struct Consts
+{
+    static constexpr float PIf = 3.1415927f;
+    static constexpr double PI = 3.14159265358979323846;
+};
+
+#endif /* Consts_h */
